8e03f06d6933b5d21fcb18fd1aaa3744fbcc5f67
Bruno <brunopasca21@gmail.com>
2022-06-11 15:48:54 -0300

    Tests unitarios piquete

---
+++ src/main/test/edu/fiuba/algo3/TestsPiquete.java
@@ -0,0 +1,38 @@
+package edu.fiuba.algo3;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestsPiquete {
+    Piquete p;
+    @BeforeEach
+    void init(){
+        this.p = new Piquete();
+    }
+
+    @Test
+    public void test01MotoChocaConPiqueteYPenalizaCon2Movimientos(){
+        Moto mockedMoto = mock(Moto.class);
+        when(mockedMoto.chocarConPiquete()).thenReturn(2);
+        assertEquals(2, p.chocar(mockedMoto));
+    }
+
+    @Test
+    public void test02AutoChocaConPiqueteYNoPenaliza(){
+        Auto mockedAuto = mock(Auto.class);
+        when(mockedAuto.chocarConPiquete()).thenReturn(0);
+        assertEquals(0, p.chocar(mockedAuto));
+    }
+
+    @Test
+    public void test034x4ChocaConPiqueteYNoPenaliza(){
+        CuatroXCuatro mocked4x4 = mock(CuatroXCuatro.class);
+        when(mocked4x4.chocarConPiquete()).thenReturn(0);
+        assertEquals(0, p.chocar(mocked4x4));
+    }
+}
+
